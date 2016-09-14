fe381397efa7673240d09563ca5a5b263fb1a3f0
Zhukov Anton Andreevich <a.zhukov1@tinkoff.ru>
2016-09-14 15:59:10 +0300

    log header changed

--- Ogam/LogTextWriter.cs
+++ Ogam/LogTextWriter.cs
@@ -1,35 +1,28 @@
-﻿    using System;
-    using System.Collections.Generic;
-    using System.Diagnostics;
-    using System.IO;
-    using System.Linq;
-    using System.Text;
-    using System.Threading;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
 
-namespace Ogam
-{
-    public class LogTextWriter : TextWriter
-    {
+namespace Ogam {
+    public class LogTextWriter : TextWriter {
         private readonly TextWriter _tw;
         private readonly Queue<string> _msgQueue;
 
-        public LogTextWriter(TextWriter tw)
-        {
+        public LogTextWriter(TextWriter tw) {
             _tw = tw;
             _msgQueue = new Queue<string>();
 
-            var bt = new Thread(() =>
-            {
+            var bt = new Thread(() => {
                 bool hasSmth = false;
-                while (true)
-                {
+                while (true) {
                     var res = string.Empty;
 
-                    lock (_msgQueue)
-                    {
+                    lock (_msgQueue) {
                         hasSmth = _msgQueue.Any();
-                        if (hasSmth)
-                        {
+                        if (hasSmth) {
                             res = _msgQueue.Dequeue();
                         }
                     }
@@ -46,151 +39,119 @@ namespace Ogam
 
         public override Encoding Encoding => _tw.Encoding;
 
-        public static string GetHeader()
-        {
-            return $"********** LOG_EVENT_MESSAGE **********->{Process.GetCurrentProcess().ProcessName}({Process.GetCurrentProcess().Id})->{DateTime.Now:HHmmss:fff}->\n";
+        public static string GetHeader() {
+            return
+                $"{Environment.NewLine}********** EVENT-{Process.GetCurrentProcess().ProcessName}[{Process.GetCurrentProcess().Id}]-{DateTime.Now:HHmmss:fff} **********{Environment.NewLine}";
         }
 
-        public override void WriteLine(string s)
-        {
+        public override void WriteLine(string s) {
             var astr = string.Format(s);
-            lock (_msgQueue)
-            {
+            lock (_msgQueue) {
                 _msgQueue.Enqueue(GetHeader() + astr);
             }
         }
 
-        public override void WriteLine(string s, object obj0)
-        {
+        public override void WriteLine(string s, object obj0) {
             var astr = string.Format(s, obj0);
-            lock (_msgQueue)
-            {
+            lock (_msgQueue) {
                 _msgQueue.Enqueue(GetHeader() + astr);
             }
         }
 
-        public override void WriteLine(string s, object obj0, object obj1)
-        {
+        public override void WriteLine(string s, object obj0, object obj1) {
             var astr = string.Format(s, obj0, obj1);
-            lock (_msgQueue)
-            {
+            lock (_msgQueue) {
                 _msgQueue.Enqueue(GetHeader() + astr);
             }
         }
 
-        public override void WriteLine(string s, object obj0, object obj1, object obj2)
-        {
+        public override void WriteLine(string s, object obj0, object obj1, object obj2) {
             var astr = string.Format(s, obj0, obj1, obj2);
-            lock (_msgQueue)
-            {
+            lock (_msgQueue) {
                 _msgQueue.Enqueue(GetHeader() + astr);
             }
         }
 
-        public override void WriteLine(string s, params object[] obj)
-        {
+        public override void WriteLine(string s, params object[] obj) {
             var astr = string.Format(s, obj);
-            lock (_msgQueue)
-            {
+            lock (_msgQueue) {
                 _msgQueue.Enqueue(GetHeader() + astr);
             }
         }
 
-        public override void WriteLine(char c)
-        {
+        public override void WriteLine(char c) {
             var astr = string.Format(c.ToString());
-            lock (_msgQueue)
-            {
+            lock (_msgQueue) {
                 _msgQueue.Enqueue(GetHeader() + astr);
             }
         }
 
-        public override void WriteLine(char[] buffer)
-        {
+        public override void WriteLine(char[] buffer) {
             var astr = string.Format(buffer.ToString());
-            lock (_msgQueue)
-            {
+            lock (_msgQueue) {
                 _msgQueue.Enqueue(GetHeader() + astr);
             }
         }
 
-        public override void WriteLine(bool b)
-        {
+        public override void WriteLine(bool b) {
             var astr = string.Format(b.ToString());
-            lock (_msgQueue)
-            {
+            lock (_msgQueue) {
                 _msgQueue.Enqueue(GetHeader() + astr);
             }
         }
 
-        public override void WriteLine(int i)
-        {
+        public override void WriteLine(int i) {
             var astr = string.Format(i.ToString());
-            lock (_msgQueue)
-            {
+            lock (_msgQueue) {
                 _msgQueue.Enqueue(GetHeader() + astr);
             }
         }
 
-        public override void WriteLine(uint i)
-        {
+        public override void WriteLine(uint i) {
             var astr = string.Format(i.ToString());
-            lock (_msgQueue)
-            {
+            lock (_msgQueue) {
                 _msgQueue.Enqueue(GetHeader() + astr);
             }
         }
 
-        public override void WriteLine(long l)
-        {
+        public override void WriteLine(long l) {
             var astr = string.Format(l.ToString());
-            lock (_msgQueue)
-            {
+            lock (_msgQueue) {
                 _msgQueue.Enqueue(GetHeader() + astr);
             }
         }
 
-        public override void WriteLine(ulong l)
-        {
+        public override void WriteLine(ulong l) {
             var astr = string.Format(l.ToString());
-            lock (_msgQueue)
-            {
+            lock (_msgQueue) {
                 _msgQueue.Enqueue(GetHeader() + astr);
             }
         }
 
-        public override void WriteLine(float f)
-        {
+        public override void WriteLine(float f) {
             var astr = string.Format(f.ToString());
-            lock (_msgQueue)
-            {
+            lock (_msgQueue) {
                 _msgQueue.Enqueue(GetHeader() + astr);
             }
         }
 
-        public override void WriteLine(double d)
-        {
+        public override void WriteLine(double d) {
             var astr = string.Format(d.ToString());
-            lock (_msgQueue)
-            {
+            lock (_msgQueue) {
                 _msgQueue.Enqueue(GetHeader() + astr);
             }
         }
 
-        public override void WriteLine(decimal dc)
-        {
+        public override void WriteLine(decimal dc) {
             var astr = string.Format(dc.ToString());
-            lock (_msgQueue)
-            {
+            lock (_msgQueue) {
                 _msgQueue.Enqueue(GetHeader() + astr);
             }
         }
 
-        public override void WriteLine(object o)
-        {
+        public override void WriteLine(object o) {
             var astr = string.Format(o.ToString());
-            lock (_msgQueue)
-            {
+            lock (_msgQueue) {
                 _msgQueue.Enqueue(GetHeader() + astr);
             }
         }
